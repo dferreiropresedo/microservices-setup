12d66446c2051a48c430b9b291f44e43ae3bc49c
Daniel Ferreiro Presedo <danielfpre@ext.inditex.com>
2024-04-08 11:44:12 +0200

    feat: Working endpoint

--- microservices/producer/src/main/java/com/learning/microservices/setup/producer/ProducerApplication.java
+++ microservices/producer/src/main/java/com/learning/microservices/setup/producer/ProducerApplication.java
@@ -8,7 +8,6 @@ public class ProducerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ProducerApplication.class, args);
-    System.out.println("It works!");
   }
 
 }
