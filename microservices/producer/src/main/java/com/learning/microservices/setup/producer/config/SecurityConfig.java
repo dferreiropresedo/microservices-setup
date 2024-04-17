20cfd89cd183d039cb29427e1c8cd0375c149bdf
Daniel Ferreiro Presedo <danielferreiro.8.10.95@gmail.com>
2024-04-17 10:33:58 +0200

    feat: Completed configuration to work both at docker and local

--- microservices/producer/src/main/java/com/learning/microservices/setup/producer/config/SecurityConfig.java
+++ microservices/producer/src/main/java/com/learning/microservices/setup/producer/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -15,15 +16,17 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain enableOauth(HttpSecurity httpSecurity) throws Exception {
     httpSecurity
+        .sessionManagement(sessionManager -> sessionManager.sessionCreationPolicy(
+            SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth ->
-            auth.requestMatchers(new AntPathRequestMatcher("/actuator/*"))
+            auth.requestMatchers(new AntPathRequestMatcher("/actuator/**"))
                 .permitAll()
                 .requestMatchers(new AntPathRequestMatcher("/hello-world"))
                 .permitAll()
                 .anyRequest()
                 .authenticated()
         );
-    httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.opaqueToken(Customizer.withDefaults()));
+    httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
     return httpSecurity.build();
   }
 
