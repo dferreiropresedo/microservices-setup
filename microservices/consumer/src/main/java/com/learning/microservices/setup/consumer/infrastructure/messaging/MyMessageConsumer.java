50fcb9cd785caa1af17edd3492d22e322f386e03
Daniel Ferreiro Presedo <danielferreiro.8.10.95@gmail.com>
2024-04-24 10:35:54 +0200

    feat: Added Schema registry and avro schemas

--- microservices/consumer/src/main/java/com/learning/microservices/setup/consumer/infrastructure/messaging/MyMessageConsumer.java
+++ microservices/consumer/src/main/java/com/learning/microservices/setup/consumer/infrastructure/messaging/MyMessageConsumer.java
@@ -1,5 +1,6 @@
 package com.learning.microservices.setup.consumer.infrastructure.messaging;
 
+import com.learning.microservices.avro.MyAvroMessage;
 import java.util.function.Consumer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
@@ -7,10 +8,10 @@ import org.springframework.stereotype.Component;
 
 @Component("my-message-consumer")
 @Slf4j
-public class MyMessageConsumer implements Consumer<Message<String>> {
+public class MyMessageConsumer implements Consumer<Message<MyAvroMessage>> {
 
   @Override
-  public void accept(Message<String> s) {
+  public void accept(Message<MyAvroMessage> s) {
     log.info("Consumed the message [{}] with headers {}", s.getPayload(), s.getHeaders());
   }
 }
--- microservices/producer/src/main/java/com/learning/microservices/setup/producer/infrastructure/messaging/MyMessageProducer.java
+++ microservices/producer/src/main/java/com/learning/microservices/setup/producer/infrastructure/messaging/MyMessageProducer.java
@@ -1,5 +1,6 @@
 package com.learning.microservices.setup.producer.infrastructure.messaging;
 
+import com.learning.microservices.avro.MyAvroMessage;
 import com.learning.microservices.setup.producer.config.beans.RandomGeneratorConfiguration;
 import com.learning.microservices.setup.producer.domain.MyMessage;
 import com.learning.microservices.setup.producer.infrastructure.RandomGenerator;
@@ -16,21 +17,25 @@ import org.springframework.stereotype.Component;
 @Component("my-message-producer")
 @Slf4j
 @RequiredArgsConstructor
-public class MyMessageProducer implements Supplier<Message<String>> {
+public class MyMessageProducer implements Supplier<Message<MyAvroMessage>> {
 
   private final RandomGeneratorConfiguration randomGeneratorConfiguration;
 
 
   @Override
-  public Message<String> get() {
+  public Message<MyAvroMessage> get() {
     MyMessage myMessage = MyMessage.builder().userId(RandomGenerator.userId())
         .message(RandomGenerator.sentence(randomGeneratorConfiguration.getKeys()))
         .createdAt(Instant.now(Clock.systemUTC()).toEpochMilli())
         .build();
-    Message<String> message = MessageBuilder.withPayload(myMessage.toString())
-        .setHeader(KafkaHeaders.KEY, myMessage.getUserId())
+    MyAvroMessage content = MyAvroMessage.newBuilder().setUserId(myMessage.getUserId())
+        .setMessage(myMessage.getMessage()).setCreatedAt(myMessage.getCreatedAt()).build();
+    
+    Message<MyAvroMessage> envelope = MessageBuilder.withPayload(content)
+        .setHeader(KafkaHeaders.KEY, content.getUserId())
         .build();
-    log.info("Sending the message: {}", message);
-    return message;
+
+    log.info("Sending the envelope: {}", envelope);
+    return envelope;
   }
 }
