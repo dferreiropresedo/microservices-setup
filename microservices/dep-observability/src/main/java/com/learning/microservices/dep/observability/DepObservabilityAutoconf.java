c24d31c11a4bf6d272bab015237578f64ecb3d56
Daniel Ferreiro Presedo <danielferreiro.8.10.95@gmail.com>
2024-05-24 12:25:39 +0200

    feat: Observability dependency

--- microservices/dep-observability/src/main/java/com/learning/microservices/dep/observability/DepObservabilityAutoconf.java
+++ microservices/dep-observability/src/main/java/com/learning/microservices/dep/observability/DepObservabilityAutoconf.java
@@ -1,13 +1,15 @@
-package com.learning.microservices.setup.producer.config;
+package com.learning.microservices.dep.observability;
 
 import io.micrometer.tracing.exporter.FinishedSpan;
 import io.micrometer.tracing.exporter.SpanExportingPredicate;
 import java.util.Optional;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class TracingConfig {
+@AutoConfiguration
+@ConditionalOnProperty(name = "tools.observability.enabled", havingValue = "true")
+public class DepObservabilityAutoconf {
 
   public static final String URI_FIELD = "uri";
 
@@ -19,5 +21,4 @@ public class TracingConfig {
   private boolean uriContainsValue(FinishedSpan span, String uriContent) {
     return Optional.ofNullable(span.getTags().get(URI_FIELD)).orElse("").contains(uriContent);
   }
-
 }
---
+++ microservices/dep-observability/src/main/java/com/learning/microservices/dep/observability/DepObservabilityYamlLoader.java
@@ -0,0 +1,69 @@
+package com.learning.microservices.dep.observability;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+
+public class DepObservabilityYamlLoader implements EnvironmentPostProcessor {
+
+  private static final DefaultResourceLoader RESOURCE_LOADER = new DefaultResourceLoader();
+
+  private static final String PATTERN = "classpath:%s.yml";
+
+  private static final String CONFIGURATION_FILE = "observability";
+
+  public static final String APPLICATION_PROPERTY_SOURCES = "my-application-property-sources";
+
+  @Override
+  public void postProcessEnvironment(ConfigurableEnvironment environment,
+      SpringApplication application) {
+    String[] activeProfiles = environment.getActiveProfiles();
+    MapPropertySource propertySource = getPropertySource(environment);
+
+    addDefaultProperties(propertySource);
+    for (String activeProfile : activeProfiles) {
+      addProfileProperties(propertySource, activeProfile);
+    }
+
+  }
+
+  private MapPropertySource getPropertySource(ConfigurableEnvironment environment) {
+    PropertySource<?> value = environment.getPropertySources().get(APPLICATION_PROPERTY_SOURCES);
+    if (value != null) {
+      return (MapPropertySource) value;
+    }
+    MapPropertySource mapPropertySource = new MapPropertySource(APPLICATION_PROPERTY_SOURCES,
+        new HashMap<>());
+    environment.getPropertySources().addLast(mapPropertySource);
+    return mapPropertySource;
+  }
+
+  private void addDefaultProperties(MapPropertySource propertySource) {
+    String configurationFileName = String.format(PATTERN, CONFIGURATION_FILE);
+    Map properties = loadProperties(configurationFileName);
+    propertySource.getSource().putAll(properties);
+  }
+
+  private void addProfileProperties(MapPropertySource propertySource, String profile) {
+    String configurationFile = CONFIGURATION_FILE + "-" + profile;
+    String configurationFileName = String.format(PATTERN, configurationFile);
+    Map properties = loadProperties(configurationFileName);
+    propertySource.getSource().putAll(properties);
+  }
+
+
+  private Properties loadProperties(String resourceName) {
+    Resource resource = RESOURCE_LOADER.getResource(resourceName);
+    YamlPropertiesFactoryBean factoryBean = new YamlPropertiesFactoryBean();
+    factoryBean.setResources(resource);
+    return factoryBean.getObject();
+  }
+}
